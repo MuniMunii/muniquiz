9fef4953722020a56ba1659c0cdd955682e45c92
MuniMunii <tyuimiti@gmail.com>
2025-06-24 19:11:49 +0700

    Setup mongodb and client

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,14 +1,18 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { allowCors } from '../../../lib/allowCors';
-
+import clientPromise from '../../../lib/mongoClient';
 async function handler(_req: VercelRequest, res: VercelResponse) {
     try{
-        console.log("test")
-        res.status(200).json({ message: "Test endpoint is working!" });
+        const client = await clientPromise;
+        const db =client.db("sample_mflix")
+        const posts=db.collection("movies")
+        const getMovies=await posts.find().limit(2).toArray()
+        console.log("test",getMovies)
+        res.status(200).json({ message: "Test endpoint is working!" ,Movie:getMovies});
     }
     catch(error){
         console.error("Error in /test endpoint:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).json({ error: "Internal Server Error",messages:error });
     }
 }
 export default allowCors(handler)
