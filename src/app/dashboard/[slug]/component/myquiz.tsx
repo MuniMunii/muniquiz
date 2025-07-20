d7bfbfeee77b7dee17cbaa8b8ca9fc644361b694
MuniMunii <tyuimiti@gmail.com>
2025-07-20 22:02:14 +0700

    Adding carousel for device width smaller than laptop and adding skeleton Loading

--- src/app/dashboard/[slug]/component/myquiz.tsx
+++ src/app/dashboard/[slug]/component/myquiz.tsx
@@ -1,27 +1,84 @@
 "use client";
-import { useState, useEffect, CSSProperties } from "react";
-import { OwnerQuizType } from "../../../../../lib/validation/quiz";
-import { GetMyQuiz } from "@/app/services/QuizService";
-import Image from "next/image";
 import useQuiz from "@/app/services/useQuiz";
+import SkeletonLoading from "@/app/component/skeleton/skeletonCard";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import useMediaQuery from "@/app/hook/useMediaQuery";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileImage } from "@fortawesome/free-regular-svg-icons";
 export default function MyQuiz() {
-  const {Quiz,isLoading,isError}=useQuiz({url:'get-myquiz'})
-  return Quiz.map((item, index) => {
-    const stripedBG: CSSProperties = {
-      backgroundImage:
-        "repeating-linear-gradient(45deg, #e1e1e1 0, #e1e1e1 1px, transparent 0, transparent 50%)",
-      backgroundSize: "10px 10px",
-      backgroundAttachment: "fixed",
-    };
+  const { Quiz, isLoading, isError } = useQuiz({ url: "get-myquiz" });
+  const isLaptop = useMediaQuery("(min-width:1024px)");
+  const isPhone = useMediaQuery("(min-width:320px)");
+  const isTablet = useMediaQuery("(min-width:764px)");
+  const stripedBG = {
+    backgroundImage:
+      "repeating-linear-gradient(45deg, #e1e1e1 0, #e1e1e1 1px, transparent 0, transparent 50%)",
+    backgroundSize: "10px 10px",
+    backgroundAttachment: "fixed",
+  };
+  const SkeletonLoadingComp=isLoading&&[1].map((_, index) => (
+            <SkeletonLoading key={`SkeletonCard-${index}`} />
+        ))
+// if (isLaptop && isLoading) {
+//   return (
+//     <div className="w-full h-fit p-4 bg-green-700 flex items-center rounded-md relative gap-4">
+//       {SkeletonLoadingComp}
+//     </div>
+//   );
+// }
+  if (isLaptop) {
     return (
       <div
-        key={item.enterID}
-        style={stripedBG}
-        className="w-48 h-52 bg-amber-50 flex flex-col justify-between rounded-md overflow-hidden"
+        className={`w-full h-fit p-4 lg 
+          ${isPhone ? "bg-green-900" : ""} 
+          ${isTablet ? "bg-green-800" : ""} 
+          ${isLaptop ? "bg-green-700" : ""} flex items-center rounded-md relative gap-4`}
       >
-        <div className="w-full h-20 text-black flex items-center"><Image src={item.image} width={32} height={32} alt={item.category+'-image'}/></div>
-        <div className="w-full h-28 bg-white"></div>
+        {isLoading&&SkeletonLoadingComp}
+        {Quiz.slice(0,5).map((item, _) => (
+          <div key={item.enterID} className="w-48 h-52 flex flex-col  justify-between overflow-hidden rounded-md">
+            <div style={stripedBG} className="w-full h-full flex items-center justify-center"><FontAwesomeIcon icon={faFileImage}/></div>
+            <div className="w-full h-52 bg-white"></div>
+          </div>
+        ))}
       </div>
     );
-  });
+  }
+  return (
+    <>
+      {" "}
+      <Carousel
+        orientation="horizontal"
+        className={`w-full h-fit p-4 lg
+          bg-green-700 
+          ${isPhone ? "bg-green-900" : ""} 
+          ${isTablet ? "bg-green-800" : ""} 
+          ${isLaptop ? "bg-green-700" : ""}
+         flex gap-4 items-center rounded-md relative`}
+      >
+        <CarouselContent className={`${!isLaptop?'ml-1':'ml-0'}`}>
+          {(isLoading&&!isLaptop)&&SkeletonLoadingComp}
+          {[1,2,3,4,5,6,7,8,9,10].map((item,_) => (
+            <CarouselItem
+              key={_}
+              className="basis-52 rounded-md overflow-hidden"
+            >
+              <div className="w-48 h-52 flex flex-col justify-between overflow-hidden rounded-md">
+<div style={stripedBG} className="w-full h-full flex items-center justify-center"><FontAwesomeIcon icon={faFileImage}/></div>
+            <div className="w-full h-52 bg-white"></div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 text-black z-10"/>
+        <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 text-black z-10"/>
+      </Carousel>
+    </>
+  );
 }
--- src/app/dashboard/[slug]/page.tsx
+++ src/app/dashboard/[slug]/page.tsx
@@ -3,11 +3,12 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../../../../lib/authOption";
 import { redirect } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faHourglass,faPenToSquare} from "@fortawesome/free-regular-svg-icons"
+import {faPenToSquare} from "@fortawesome/free-regular-svg-icons"
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import MyQuiz from "./component/myquiz";
 import JoinInput from "@/app/component/joinInput";
+import { useSession } from "next-auth/react";
 export default async function DashboardUserPage({params}:{params:Promise<{slug:string}>}) {
   const {slug} =await params;
   const session=await getServerSession(authOptions)
@@ -25,12 +26,12 @@ export default async function DashboardUserPage({params}:{params:Promise<{slug:s
       {/*  */}
       <div className="max-w-[1200px] w-[90%] mx-auto mt-7">
         {/* profile info/search code */}
-        <div className="w-full h-fit bg-white border-gray-400 rounded-md max-md:flex-col-reverse flex p-10 gap-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+        <div className="w-full h-fit bg-white border-gray-400 rounded-md max-md:flex-col-reverse flex p-10 gap-3 shadow-main">
           <div className="flex items-center w-[60%] max-md:w-full text-black gap-3">
             <JoinInput/>
           </div>
           {/* profile */}
-          <div className="h-full w-2/5 max-md:w-full bg-violet-200 rounded-md flex-col flex p-4 gap-6 justify-between shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+          <div className="h-full w-2/5 max-md:w-full bg-violet-200 rounded-md flex-col flex p-4 gap-6 justify-between shadow-main">
           <h1 className="text-xl text-slate-800">Hello, {user?.username}</h1>
           {user?.image?<Image src={user?.image} alt="User Profile" width={120} height={120} className="rounded-full self-center"/>:''}
           <Button type="button"className="flex gap-3 items-center bg-indigo-500 hover:bg-indigo-600 w-fit py-1.5 px-2.5 rounded-md"><FontAwesomeIcon icon={faPenToSquare}/>Edit Profile</Button>
@@ -38,12 +39,7 @@ export default async function DashboardUserPage({params}:{params:Promise<{slug:s
         </div>
         <div className="w-full mt-3">
           <h2 className="text-3xl text-black">Recent Quiz</h2>
-          <div className="w-full h-fit p-2 bg-amber-300 flex gap-3 items-center rounded-md">
-            <div className="w-48 h-32 rounded-md bg-amber-50"></div>
-            <div className="w-48 h-32 rounded-md bg-amber-50"></div>
-            <div className="w-48 h-32 rounded-md bg-amber-50"></div>
             <MyQuiz />
-          </div>
         </div>
       </div>
     </div>
